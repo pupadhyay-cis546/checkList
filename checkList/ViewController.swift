db9f1787c5345aa5ba6d24ee97f75c2d9fa96f0b
Upadhyay, Priyanka <pupadhyay@csis-imac-23640.local>
2017-04-26 03:22:24 -0500

    checked

--- checkList/ViewController.swift
+++ checkList/ViewController.swift
@@ -8,8 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    
+    let foods = ["tomato", "apple", "cheese", "milk", "potato"]
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foods.count
+    }
+    override func tableView(_ _tableView: UITableView, cellForRowAt indexPath: IndexPath)-> UITableViewCell
+    {
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = foods[indexPath.row]
+        return cell
+    }
+    
+    
+    //to check
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCellAccessoryType.checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.none
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.checkmark
+        }
+    }
+    
+    
 
+        
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
